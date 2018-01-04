d6fd343fec1baa4ed7bc37a203f68c2e10111d4d
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-01-04 17:08:40 -0500

    separated javascript out of html files. adjusted modal design

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,37 @@ var friends = [
 {
 	name: `Justine`,
 	age: 30,
-	photo: `../data/justine.jpg`,
+	gender: `female`,
+	photo: `../data/justine.jpg70`,
 	scores: [5, 2, 3, 5, 4, 1] 
 }, {
 	name: `Keisha`,
 	age: 27,
+	gender: `female`,
 	photo: `../data/keisha.jpg`,
 	scores: [3, 4, 5, 5, 4, 3] 
 }, {
 	name: `Rosario`,
 	age: 33,
+	gender: `female`,
 	photo: `../data/rosario_dawson.jpg`,
 	scores: [1, 4, 4, 5, 4, 3] 
 }, {
 	name: `Gary`,
 	age: 29,
+	gender: `male`,
 	photo: `../data/gary.jpg`,
 	scores: [5, 2, 3, 2, 4, 4] 
 }, {
 	name: `James`,
 	age: 35,
+	gender: `male`,
 	photo: `../data/james_franco.jpg`,
 	scores: [1, 2, 4, 5, 4, 4] 
 }, {
 	name: `Idris`,
 	age: 41,
+	gender: `male`,
 	photo: `../data/idris_elba.jpg`,
 	scores: [4, 3, 3, 2, 1, 1] 
 }
---
+++ app/public/main.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+var bounceIn = 'bounceInDown';
+	var bounce = 'bounce';
+	var pulse = 'pulse';
+	var animateInfinite = '-webkit-animation-iteration-count';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$(document).one(animationEnd, function(){
+		$('#heart-text').css('display', 'block').addClass('fadeIn');
+		animation();
+	})
+function animation(){
+	var bounceStart = 
+		setInterval(function(){
+			$('.heart-sect').addClass(bounce);}, 2000);
+	var bounceEnd = 
+		setInterval(function(){
+			$('.heart-sect').removeClass(bounce);}, 4000);
+	function pulseOn(){
+		$(this).removeClass(bounceIn);
+		clearInterval(bounceStart);
+		clearInterval(bounceEnd);
+		$('.heart-sect').addClass(pulse);
+		$('.heart-sect').css(animateInfinite, 'infinite');
+	}
+	function pulseOff(){
+		$('.heart-sect').removeClass(pulse);
+		$('.heart-sect').css(animateInfinite, '');
+		bounceStart = setInterval(function(){
+			$('.heart-sect').addClass(bounce);}, 2000);
+		bounceEnd = 
+		setInterval(function(){
+			$('.heart-sect').removeClass(bounce);}, 4000);
+	}
+	$('#heart').hover(pulseOn, pulseOff);
+	$('#heart-text').hover(pulseOn, pulseOff);
+	$('#saveForm').on('click', function(){
+		alert('you clicked me?!')
+	})
+}
+
+
+$('#submit').on('click', function(event){
+		event.preventDefault();
+		var q1 = parseFloat($('#question-1').val());
+		var q2 = parseFloat($('#question-2').val());
+		var q3 = parseFloat($('#question-3').val());
+		var q4 = parseFloat($('#question-4').val());
+		var q5 = parseFloat($('#question-5').val());
+		var q6 = parseFloat($('#question-6').val());
+		var userName = $('#userName').val();
+		var userGender = $('#gender').val();
+		var userScore = {
+			name: userName,
+			gender: userGender,
+			scores: [q1, q2, q3, q4, q5, q6]
+		};
+		function checkSubmission(score){
+			return score > 0;
+		}
+		console.log(userName);
+		console.log(userGender);
+		console.log(userScore.scores.every(checkSubmission))
+		if (userName === ''|| userGender === 'Gender'){
+			console.log('you missed something');
+		}else if (!userScore.scores.every(checkSubmission)){
+			console.log('you missed one');
+		}else{
+			console.log(userScore);
+
+			$.post('/api/friends', userScore, function(data){
+					if(data){
+						alert('Please wait while we find your match! :)')
+					}
+				
+				}).then(function(data){
+					console.log(data);
+					var matchPic = `<img class="modal-img" src='${data.photo}'>`;
+					var matchName = data.name;
+					$('.modal-body').html(matchPic);
+					$('#modal-subtitle').html(`Meet ${matchName}`);
+					$('#myModal').modal('toggle');
+
+				});
+		}
+
+	});
+
+})
